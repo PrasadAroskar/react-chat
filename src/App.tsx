f07fa6a13cde642c6b7aa6af2a84a5017e4f3847
Prasad Aroskar <aroskar.p@northeastern.edu>
2021-11-03 13:14:01 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Form from "@athena/forge/Form";
+import Root from "@athena/forge/Root";
+import "@athena/forge/dist/forge.css";
+import Textarea from "@athena/forge/Textarea";
+import FormField from "@athena/forge/FormField";
+
+export function App() {
+  return (
+    <Root>
+      <h1>Chat</h1>
+      <Form>
+        <FormField labelText="Message" id="message" inputAs={Textarea} />
+      </Form>
+    </Root>
+  );
+}
